c0a63b58b03fc2f5aa727b0deff31047a77ca337
Joao <joao.teixeira@aedb.br>
2021-09-29 17:18:23 -0300

    configurando o axios e a url da Api

---
+++ src/services/api.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const BASE_URL = 'https://pokeapi.co/api/v2/';
+
+export const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    Accept: "application/json",
+  }
+})
