ef1b50ba8992e4f1c98b402cd5cbc308e15c0598
Joao <joao.teixeira@aedb.br>
2021-09-30 16:52:07 -0300

    configurando o Sidebar para recebe os dados do redux

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,28 +1,61 @@
-import React from 'react';
+import React, { useState, useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
-import plusImg from '../../assets/img/plus.png';
+import PokemonRedux from "../../redux/ducks/pokemon";
+import plusImg from "../../assets/img/plus.png";
 
-import { Button } from '../Button';
-import { Container, Item } from './styles';
+import { ModalPokemon } from "../ModalPokemon";
+import { Button } from "../Button";
+import { Container, Item } from "./styles";
 
-export function SideBar({data, onClick}) {
-  return(
+export function SideBar({ data, setPokebolas }) {
+  const dispatch = useDispatch();
+  const { data: pokemonSelect } = useSelector(({ pokemon }) => pokemon);
+ 
+  const [openModal, setOpenModal] = useState(false);
+
+  async function handleSubmit(id) {
+    try {
+      if (id !== null) {
+        await dispatch(PokemonRedux.captureRequest(id));
+
+        setOpenModal(true);
+      }
+    } catch (e) {}
+  }
+
+  const handleCloseModal = useCallback(() => {
+    setOpenModal(false);
+  }, []);
+
+  return (
     <Container>
       {data.length <= 6 &&
-        data.map((pokemon) => {
-          <Item key={pokemon.id} src={pokemon?.sprites?.front_default} />
-        })
-      }
+        data.map((pokemon) => (
+          <Item
+            key={pokemon.id}
+            onClick={() => handleSubmit(pokemon.id)}
+            src={pokemon?.sprites?.front_default}
+          />
+        ))}
 
       {data.length < 6 && <Item>?</Item>}
 
-      {data.length < 6 && 
-        <Button 
-          onClick={onClick}
-          icon={<img src={plusImg} alt="add" />} 
+      {data.length < 6 && (
+        <Button
+          onClick={() => console.log("botão create")}
+          icon={<img src={plusImg} alt="add" />}
         />
-      }
-  
+      )}
+
+      <ModalPokemon
+        isOpen={openModal}
+        onRequestClose={handleCloseModal}
+        setPokebolas={setPokebolas}
+        data={pokemonSelect}
+        arryPokemon={data}
+        isEdit
+      />
     </Container>
-  )
+  );
 }
