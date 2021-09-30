be723a535133f6478ade76f1e3794b15a4a3becc
Joao <joao.teixeira@aedb.br>
2021-09-30 16:48:20 -0300

    configurando o index do redux

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,1 +1,36 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+import reducers from './ducks';
+import sagas from './sagas';
+
+const config = {
+  key: 'pokemon',
+  whitelist: [],
+  storage,
+};
+
+const persistedRedurcer = persistReducer(config, reducers);
+
+const sagaMonitor = process.env.NODE_ENV === "development"
+? console.tron.createSagaMonitor()
+: null;
+
+const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+const middlewares = [];
+
+middlewares.push(sagaMiddleware);
+
+const composer = process.env.NODE_ENV === "development"
+  ? compose(applyMiddleware(...middlewares), console.tron.createEnhancer())
+  : compose(applyMiddleware(...middlewares));
+
+const store = createStore(persistedRedurcer, composer);
+const persistor = persistStore(store);
+
+sagaMiddleware.run(sagas);
+
+export { store, persistor };
