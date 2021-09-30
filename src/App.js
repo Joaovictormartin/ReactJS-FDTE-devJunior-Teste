1c4914bf6bfc74152bed4a6d8f6ba637c044a307
Joao <joao.teixeira@aedb.br>
2021-09-30 16:50:08 -0300

    adicionando o provider do redux no app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react";
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
 
-import { GlobalStyle } from './styles/global';
-import { Routes } from './routes';
+import './utils/Reactotron';
+
+import { Routes } from "./routes";
+import { store, persistor } from "./redux";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <BrowserRouter forceRefresh basename="/">
-      <Routes/>
-      <GlobalStyle />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter forceRefresh basename="/">
+          <Routes />
+          <GlobalStyle />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   );
 }
